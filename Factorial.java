1a1222fee6944873feccbf0e1e8d4d5dfc775cb6
Santosh Faculty <santoshfaculty1@gmail.com>
2014-10-31 20:49:24 +0530

    Added code

--- Factorial.java
+++ Factorial.java
@@ -1,1 +1,18 @@
-//I will solve factorial problem here
+// My Factorial Assessment
+
+public class Factorial(){
+	public static void main(String av[]){
+		System.out.println("Factorial "+simpleFactorial(7) );
+	}
+	public static int simpleFactorial(int n){
+        	int result = 1;
+       		for(int i = 1; i <= n; i++){
+            		result *= i;
+        	}
+        return result;
+    }
+
+
+}
+
+
